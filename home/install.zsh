a3f74dc8233434d65c1d986c30a25078a1bd0a8d
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-21 00:16:10 +0200

    Update install.zsh

--- home/install.zsh
+++ home/install.zsh
@@ -2,12 +2,5 @@ mkdir -p $HOME/Code
 mkdir -p $HOME/Code/bin
 mkdir -p $HOME/Code/tmp
 
-if cd $HOME/Code/dotfiles
-then
-    git pull origin master
-else
-    git clone git@github.com:AdamAmanbaev/dotfiles.git $HOME/Code/dotfiles
-fi
-
 git config --global user.email "adam.amanbaev@gmail.com"
 git config --global user.name "Adam Amanbaev"
