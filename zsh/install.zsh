bf96f78ee3e411f31295ed0116eda4fe0359f338
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-21 01:09:55 +0200

    Update install.zsh

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -1,7 +1,2 @@
-if ! cat /etc/passwd | grep $USER | grep "/usr/bin/zsh$" >/dev/null
-then
-which zsh>/dev/null && chsh -s `which zsh`
-fi
-
 rm -f $HOME/.zshrc
 ln -s $HOME/Code/dotfiles/zsh/zshrc $HOME/.zshrc
