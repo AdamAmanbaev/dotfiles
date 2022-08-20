358e18965d72a10b92437990e77cc5b52f8f6310
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-21 00:23:14 +0200

    Update install.zsh

--- contest/install.zsh
+++ contest/install.zsh
@@ -1,5 +1,4 @@
 if [[ want_home ]] {
     mkdir -p $HOME/Code/contest
-    mkdir -p $HOME/Code/contest/library
     git clone git@github.com:AdamAmanbaev/Competitive_Programming.git $HOME/Code/contest
 }
