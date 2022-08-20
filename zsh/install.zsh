b57db0e387dee9633df402ec15684f0949d55f79
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-20 22:28:01 +0200

    Create install.zsh

---
+++ zsh/install.zsh
@@ -0,0 +1,7 @@
+if ! cat /etc/passwd | grep $USER | grep "/usr/bin/zsh$" >/dev/null
+then
+which zsh>/dev/null && chsh -s `which zsh`
+fi
+
+rm -f $HOME/.zshrc
+ln -s $HOME/Code/dotfiles/zsh/zshrc $HOME/.zshrc
