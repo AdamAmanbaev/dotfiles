b3c142908cdeea31de9600ecf18c6f117b27d2a6
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-20 23:02:19 +0200

    Update install.zsh

--- contest/install.zsh
+++ contest/install.zsh
@@ -1,6 +1,5 @@
 if [[ want_home ]] {
     mkdir -p $HOME/Code/contest
     mkdir -p $HOME/Code/contest/library
-    mv $HOME/Code/dotfiles/template.cpp $HOME/Code/contest/library/template
-    mv $HOME/Code/dotfiles/longtemplate.cpp $HOME/Code/contest/library/template
+    git clone git@github.com:AdamAmanbaev/Competitive_Programming/vimcp/library $HOME/Code/contest/library
 }
