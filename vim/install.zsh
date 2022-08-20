5ee38888e6ae4a1f20ace288989d58dc5454978c
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-20 21:45:28 +0200

    Update install.zsh

--- vim/install.zsh
+++ vim/install.zsh
@@ -16,3 +16,6 @@ then
 else 
     git clone git://github.com/mh21/errormarker.vim $HOME/.vim/bundle
 fi
+
+mkdir -p ~/.vim/autoload && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
