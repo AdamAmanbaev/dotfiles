f9da08c5b7b018cca7a91b0143c0d3c37ebbcd2f
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-21 03:06:59 +0200

    Update install.zsh

--- packages/install.zsh
+++ packages/install.zsh
@@ -6,13 +6,13 @@ if want_basic
 then
     BASIC_PACKAGES=(vim python3 python3-pip git zsh curl)
     echo "Installing basic packages ($BASIC_PACKAGES)"
-    sudo apt-get install $BASIC_PACKAGES
+    sudo apt-get install -y $BASIC_PACKAGES
 fi
  
 if want_dev
 then
     DEV_PACKAGES=(g++ gcc cmake neofetch lolcat)
     echo "Installing dev packages ($DEV_PACKAGES)"
-    sudo apt-get install $DEV_PACKAGES
+    sudo apt-get install -y $DEV_PACKAGES
 fi
 
