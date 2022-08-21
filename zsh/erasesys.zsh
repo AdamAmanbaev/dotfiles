2b98863b6e540b9d4d6c653059e8af8dcb55c835
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-21 13:29:58 +0200

    Update erasesys.zsh

--- zsh/erasesys.zsh
+++ zsh/erasesys.zsh
@@ -1,3 +1,11 @@
+while true; do 
+    read -p "Have you moved this script to /home [Y/n]? " YN
+    case $YN in 
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+    esac
+done 
+
 while true; do
     read -p "Do you wish to erase system and user [Y/n]? " yn
     case $yn in
