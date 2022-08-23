2c3874b08e54969cd2d80a6dd85d30d9d0db465e
Adam Amanbaev <71196708+AdamAmanbaev@users.noreply.github.com>
2022-08-23 15:21:17 +0200

    Update editor.sh

--- bash/editor.sh
+++ bash/editor.sh
@@ -116,17 +116,8 @@ if ! shopt -oq posix; then
   fi
 fi
 
-#custom
-ulimit -s unlimited  # stack-limit
-export PATH=$PATH:/mnt/c/users/adama/AppData/Local/SumatraPDF/
-export PATH=$PATH:/usr/local/stata16/
-alias exp='/mnt/c/WINDOWS/explorer.exe'
-
-PATH=$(/usr/bin/printenv PATH | /usr/bin/perl -ne 'print join(":", grep { !/\/mnt\/[a-z]/ } split(/:/));')
-
-export PATH="$(echo "$PATH" | python3 -c "import sys; path = sys.stdin.read().split(':'); path = [pp for pp in path if '/mnt/c' not in pp]; print(':'.join(path))")"
-export LD_LIBRARY_PATH="$(echo "$LD_LIBRARY_PATH" | python3 -c "import sys; path = sys.stdin.read().split(':'); path = [pp for pp in path if '/mnt/c' not in pp]; print(':'.join(path))")"
-
+# turn off login prompt
 touch $HOME/.hushlogin
 
+# Nice logo and info when starting terminal
 neofetch --ascii_distro arch linux | lolcat
