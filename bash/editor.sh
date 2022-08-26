56d12a3a25d0a8bfb9d65e12a09983a59c77cf3a
Adam Amanbaev <adam.amanbaev@gmail.com>
2022-08-26 09:12:06 +0200

    latex paths

--- bash/editor.sh
+++ bash/editor.sh
@@ -121,3 +121,13 @@ touch $HOME/.hushlogin
 
 # Nice logo and info when starting terminal
 neofetch --ascii_distro arch linux | lolcat
+
+ulimit -s unlimited  # stack-limit
+export PATH=$PATH:/mnt/c/users/adamamanbaev/AppData/Local/SumatraPDF/
+export PATH=$PATH:/usr/local/stata16/
+alias exp='/mnt/c/WINDOWS/explorer.exe'
+
+PATH=$(/usr/bin/printenv PATH | /usr/bin/perl -ne 'print join(":", grep { !/\/mnt\/[a-z]/ } split(/:/));')
+
+export PATH="$(echo "$PATH" | python3 -c "import sys; path = sys.stdin.read().split(':'); path = [pp for pp in path if '/mnt/c' not in pp]; print(':'.join(path))")"
+export LD_LIBRARY_PATH="$(echo "$LD_LIBRARY_PATH" | python3 -c "import sys; path = sys.stdin.read().split(':'); path = [pp for pp in path if '/mnt/c' not in pp]; print(':'.join(path))")"
