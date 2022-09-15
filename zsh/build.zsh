d25ea3e93a8ab05f8c9aaf8952c5c4adf33672d0
Adam Amanbaev <adam.amanbaev@gmail.com>
2022-09-15 09:39:03 +0200

    added W flags

--- zsh/build.zsh
+++ zsh/build.zsh
@@ -1,1 +1,1 @@
-g++ -DLOCAL -DDEBUG -fsanitize=address -fsanitize=undefined -O3 -std=c++20 -o $1 $1.cpp
+g++ -DLOCAL -DDEBUG -Werror -Wall -Wextra -fsanitize=address -fsanitize=undefined -O3 -std=c++20 -o $1 $1.cpp
