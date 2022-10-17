2d942a66883b60d8ec045e289cd4be413a97fa93
Adam Amanbaev <71196708+chopingu@users.noreply.github.com>
2022-10-17 19:18:09 +0200

    Update install.zsh
    change username

--- contest/install.zsh
+++ contest/install.zsh
@@ -1,4 +1,4 @@
 if [[ want_home ]] {
     mkdir -p $HOME/Code/contest
-    git clone git@github.com:AdamAmanbaev/cp-library.git $HOME/Code/contest
+    git clone git@github.com:chopingu/cp-library.git $HOME/Code/contest
 }
