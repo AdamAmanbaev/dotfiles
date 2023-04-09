fef2ae70f2c79fc592ae0f2cb021a8604117dd93
Adam Amanbaev <adam.amanbaev@gmail.com>
2023-04-09 20:16:26 +0200

    Export colors to tmux

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -12,3 +12,6 @@ export PATH=$PATH:~/Code/dotfiles/zsh/stress
 PATH=$(/usr/bin/printenv PATH | /usr/bin/perl -ne 'print join(":", grep { !/\/mnt\/[a-z]/ } split(/:/));')
 export PATH="$(echo "$PATH" | python3 -c "import sys; path = sys.stdin.read().split(':'); path = [pp for pp in path if '/mnt/c' not in pp]; print(':'.join(path))")"
 export LD_LIBRARY_PATH="$(echo "$LD_LIBRARY_PATH" | python3 -c "import sys; path = sys.stdin.read().split(':'); path = [pp for pp in path if '/mnt/c' not in pp]; print(':'.join(path))")"
+
+# exprt colors to tmux
+export TERM="xterm-256color"
