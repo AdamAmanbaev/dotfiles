bcfa7a69622664169193440642497a49104ce89a
Adam Amanbaev <71196708+chopingu@users.noreply.github.com>
2023-08-24 12:42:15 +0200

    Fix wget url

--- packages/install.zsh
+++ packages/install.zsh
@@ -33,7 +33,7 @@ echo "Installing gef for gdb"
 bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 
 echo "Installing lsd"
-wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb/
+wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb
 sudo dpkg -i lsd_0.23.1_amd64.deb
 rm lsd_0.23.1_amd64.deb
 
