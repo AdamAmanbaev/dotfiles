088a2dab20573a02f02b9a71ef5985ec64c88589
Adam Amanbaev <71196708+chopingu@users.noreply.github.com>
2023-08-24 12:48:29 +0200

    Update install.zsh

--- packages/install.zsh
+++ packages/install.zsh
@@ -39,7 +39,7 @@ rm lsd_0.23.1_amd64.deb
 
 echo "Setting up docker"
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
@@ -48,8 +48,8 @@ echo \
      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo apt-get install docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-compose
 sudo gpasswd -a $USER docker
 newgrp docker
 
